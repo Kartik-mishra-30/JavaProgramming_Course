a5115f73fedff5ba6dbeda96d4a675807dd7c643
Kartik Mishra <kartikmishra3007j@gmail.com>
2026-04-07 13:24:23 +0530

    Day 3 learnings

--- Basics/floats.java
+++ Basics/floats.java
@@ -9,18 +9,16 @@ public class floats {
        System.out.printf("Value of f: %.20f%n", f);//--Value of f: 0.69999998807907100000....why?
        System.out.println(f);//--> Prints 0.7.. why?
 
-       //Conclusion -->0.699999988079071000...is how 0.7 is stored in memory,we print normally it gives us round off value.
+       //Conclusion -->0.699999988079071000...is how 0.7 is stored in memory,we print normally it gives us round off value.. why?
+       //detailed in notes. about how 32 bit in float is divided to how it stores and reads the binary code and its form and Bias.
 
        // How Negative numbers are stored in Java?
        
        byte c = -42;
-       System.out.println(c);
-
-
+       System.out.println(c);// we only have 0 and 1 , so how computer stores negative number?
+       // detailed in notes.about how it uses 1st,2nd complement, MSB,LSB, 
        
 
-       
-       
-       
+       // I put keywords in these comments, so revising becomes easy :)
     }
 }   
